d06b975710ade857ac3705cf1bbfffe8d39f0dc3
M. Andrés Piana <153022165+MateoAPiana@users.noreply.github.com>
2024-10-09 07:49:36 -0300

    Fix the ortography

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <header>
-      <h1>Encuesta a cerca de la Inteligencia Artificial</h1>
+      <h1>Encuesta acerca de la Inteligencia Artificial</h1>
     </header>
   );
 }
